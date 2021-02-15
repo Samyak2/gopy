7f72de449be7c9a61db4dfcb509d052826a931eb
Srinidhi Temkar <srinidhitemkar6699@gmail.com>
2021-02-15 23:56:04 +0530

    lexer - adds semicolon tokens at appropriate places

--- go_lex.py
+++ go_lex.py
@@ -10,27 +10,52 @@ from symbol_table import SymbolTable
 colorama.init()
 
 
-def find_column(inp_str, token):
+with open(sys.argv[1], "r") as f:
+    input_code = f.read()
+
+if input_code[len(input_code)-1] != '\n':
+    input_code += '\n'
+
+
+insertsemi = False
+
+def set_insertsemi():
+    global insertsemi
+    if input_code[lexer.lexpos] == '\n':
+        insertsemi = True
+
+
+def find_column(token):
     """Find column number of token"""
-    line_start = inp_str.rfind("\n", 0, token.lexpos) + 1
+    line_start = input_code.rfind("\n", 0, token.lexpos) + 1
     return (token.lexpos - line_start) + 1
 
 
 # List of token names.   This is always required
 tokens = (
-    # operators
+    # punctuation
+    "SEMICOLON",
     "COMMA",
     "DOT",
+
+    # assignment operators
     "EQUAL",
     "WALRUS",
+    
+    # arithmetic operators
+    "INCREMENT",
+    "DECREMENT", 
     "PLUS",
     "MINUS",
     "MULTIPLY",
     "DIVIDE",
+    "MODULO",
+
     # literals
     "INT_LITERAL",
     "FLOAT_LITERAL",
     "STRING",
+    
     # parenthesis
     "ROUND_START",
     "ROUND_END",
@@ -39,8 +64,6 @@ tokens = (
     "SQ_START",
     "SQ_END",
     "IDENTIFIER",
-    "SINGLE_COMMENT",
-    "MULTI_COMMENT",
 )
 
 keywords = {
@@ -75,64 +98,155 @@ types = {"int": ("INT", 8), "float64": ("FLOAT64", 8), "bool": ("BOOL", 1)}
 
 tokens = tokens + tuple(keywords.values()) + tuple(i[0] for i in types.values())
 
-# Operators
+
+## tokens to ignore
+
+t_ignore_SPACES = r'\s+'
+t_ignore_TABS = r'\t+'
+t_ignore_SINGLE_COMMENT =  r'//.*'
+t_ignore_MULTI_COMMENT = r'/\*(.|\n)*?\*/'
+
+
+## tokens with no actions
+
+# punctuation
+t_SEMICOLON = r";"
 t_COMMA = r","
 t_DOT = r"\."
+# assignment operators
 t_EQUAL = r"="
 t_WALRUS = r":="
+# arithmetic operators
 t_PLUS = r"\+"
 t_MINUS = r"\-"
 t_MULTIPLY = r"\*"
 t_DIVIDE = r"/"
+t_MODULO = r"%"
+# types
+t_INT = r"int"
+t_FLOAT64 = r"float64"
+t_BOOL = r"bool"
+# opening parentheses
+t_ROUND_START = r'\('
+t_SQ_START = r'\['
+t_CURL_START = r'\{'
 
-# Literals
-# t_INT_LITERAL = r"[0-9]+"
-# t_FLOAT_LITERAL = r"[0-9]*\.[0-9]+"
+
+## tokens with actions
+
+
+# newline characters
+def t_NEWLINE(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value) # track line numbers
+
+    global insertsemi
+    if insertsemi:
+        insertsemi = False
+        semi_tok = lex.LexToken()
+        semi_tok.type = 'SEMICOLON'
+        semi_tok.value = ';'
+        semi_tok.lineno = t.lexer.lineno
+        semi_tok.lexpos = t.lexer.lexpos
+        return semi_tok
+
+
+# closing parantheses
+
+def t_ROUND_END(t):
+    r'\)'
+
+    set_insertsemi()
+    return t
+
+def t_SQ_END(t):
+    r'\]'
+
+    set_insertsemi()
+    return t
+
+def t_CURL_END(t):
+    r'\}'
+
+    set_insertsemi()
+    return t    
+
+
+# keywords
+
+def t_BREAK(t):
+    r'break'
+
+    set_insertsemi()
+    return t
+
+def t_CONTINUE(t):
+    r'continue'
+
+    set_insertsemi()
+    return t
+
+def t_FALLTHROUGH(t):
+    r'fallthrough'
+
+    set_insertsemi()
+    return t
+
+def t_RETURN(t):
+    r'return'
+
+    set_insertsemi()
+    return t
+
+
+# increment/decrement operators
+
+def t_INCREMENT(t):
+    r'\+\+'
+
+    set_insertsemi()
+    return t
+
+def t_DECREMENT(t):
+    r'--'
+
+    set_insertsemi()
+    return t
+
+
+# literals
+
 def t_STRING(t):
-    r"\"[^\"]*\""
+    r'\"[^\"]*\"'
+    
     t.value = ("string", t.value)
+    
+    set_insertsemi()
     return t
 
 
 def t_FLOAT_LITERAL(t):
     r"\d*\.\d+"
+    
     t.value = ("float64", float(t.value))
+    
+    set_insertsemi()
     return t
 
 
 def t_INT_LITERAL(t):
     r"\d+"
+    
     t.value = ("int", int(t.value))
+    
+    set_insertsemi()
     return t
-
-
-# types
-t_INT = r"int"
-t_FLOAT64 = r"float64"
-t_BOOL = r"bool"
-
-# parenthesis
-t_ROUND_START = r"\("
-t_ROUND_END = r"\)"
-
-
-def t_CURL_START(t):
-    r"\{"
-    symtab.depth += 1
-
-
-def t_CURL_END(t):
-    r"\}"
-    symtab.depth -= 1
-
-
-t_SQ_START = r"\["
-t_SQ_END = r"\]"
 
 
 # identifier
 def t_IDENTIFIER(t):
     r"([a-zA-Z]([a-zA-Z0-9_])*)|_"
+    
     if t.value in keywords:
         t.type = keywords[t.value]
     elif t.value in types:
@@ -141,27 +255,9 @@ def t_IDENTIFIER(t):
         t.type = "IDENTIFIER"
         symtab.add(t.value)
         t.value = ("identifier", t.value)
-
+    
+    set_insertsemi()
     return t
-
-
-# comments
-def t_SINGLE_COMMENT(t):
-    "//.*"
-
-
-def t_MULTI_COMMENT(t):
-    r"/\*(.|\n)*?\*/"
-
-
-# Define a rule so we can track line numbers
-def t_newline(t):
-    r"\n+"
-    t.lexer.lineno += len(t.value)
-
-
-# A string containing ignored characters (spaces and tabs)
-t_ignore = " \t"
 
 
 def print_error(err_str):
@@ -171,11 +267,11 @@ def print_error(err_str):
 # Error handling rule
 def t_error(t):
     print_error(f"Illegal character {t.value[0]}")
-    col = find_column(input_, t)
+    col = find_column(t)
     print(f"at line {t.lineno}, column {col}")
     print(
         f"{Fore.GREEN}{t.lineno:>10}:\t{Style.RESET_ALL}",
-        lines[t.lineno - 1],
+        # lines[t.lineno - 1],
         sep="",
     )
     print(" " * 10, " \t", " " * (col - 1), "^", sep="")
@@ -185,20 +281,15 @@ def t_error(t):
 
 # Build the lexer
 lexer = lex.lex()
-input_ = ""
+
+# Give input to the lexer
+lexer.input(input_code)
+
 lines = []
 symtab = SymbolTable()
 
 
-# Give the lexer some input
-with open(sys.argv[1], "rt") as f:
-    lexer.input(f.read())
-
-
 if __name__ == "__main__":
     # Tokenize
-    while True:
-        tok = lexer.token()
-        if not tok:
-            break  # No more input
+    for tok in lexer:
         print(tok)
