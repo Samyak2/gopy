fba5228ce2c8ad3a7f1c3e84873065af6e7e5e97
Srinidhi Temkar <srinidhitemkar6699@gmail.com>
2021-02-15 22:26:29 +0530

    moved symbol table to a new module

--- go_lex.py
+++ go_lex.py
@@ -1,13 +1,11 @@
 import sys
-from dataclasses import dataclass, field
-from typing import Dict, Tuple, Any
 
 import colorama
 from colorama import Fore, Style
-from tabulate import tabulate
 
 from ply import lex
-from utils import Node
+
+from symbol_table import SymbolTable
 
 colorama.init()
 
@@ -16,83 +14,6 @@ def find_column(inp_str, token):
     """Find column number of token"""
     line_start = inp_str.rfind("\n", 0, token.lexpos) + 1
     return (token.lexpos - line_start) + 1
-
-
-@dataclass
-class SymbolInfo:
-    """Stores information related to a symbol"""
-
-    depth: int
-    lineno: int = None
-    type_: str = None
-    storage: int = None
-    symbol: Node = None
-    const: bool = False
-    value: Any = None
-    uses: list = field(default_factory=list)
-
-
-class SymbolTable:
-    """Stores all identifiers, literals and information
-    related to them"""
-
-    def __init__(self):
-        self.mapping: Dict[Tuple[str, int], SymbolInfo] = {}
-        self.depth = 0
-
-    def add(self, symbol: str) -> SymbolInfo:
-        if (symbol, self.depth) in self.mapping:
-            return self.mapping[symbol, self.depth]
-
-        new_symbol = SymbolInfo(self.depth)
-        self.mapping[symbol, self.depth] = new_symbol
-
-        return new_symbol
-
-    def update_info(self, symbol: str, lineno, type_=None, const=None, value=None):
-        sym = self.mapping[symbol, self.depth]
-        sym.lineno = lineno
-        sym.type_ = type_
-        # TODO: infer type from value if not given
-        # and set storage appropriately
-        # sym.storage = types[type_][1]
-        if value is not None:
-            sym.value = value
-
-        if const is not None:
-            sym.const = const
-
-    def check_exists(self, symbol) -> bool:
-        return (symbol, self.depth) in self.mapping
-
-    def is_declared(self, symbol) -> bool:
-        return (
-            self.check_exists(symbol)
-            and self.mapping[symbol, self.depth].lineno is not None
-        )
-
-    def get_if_exists(self, symbol) -> SymbolInfo:
-        return self.mapping.get((symbol, self.depth), None)
-
-    def __str__(self):
-        return str(
-            tabulate(
-                [
-                    [
-                        key[0],
-                        key[1],
-                        value.lineno,
-                        value.type_,
-                        value.storage,
-                        value.value,
-                        value.uses,
-                    ]
-                    for key, value in self.mapping.items()
-                ],
-                headers=["Symbol", "Depth", "Line No.", "Type", "Storage", "Value", "Uses"],
-                tablefmt="fancy_grid",
-            )
-        )
 
 
 # List of token names.   This is always required
---
+++ symbol_table.py
@@ -0,0 +1,80 @@
+from dataclasses import dataclass, field
+from tabulate import tabulate
+from utils import Node
+from typing import Dict, Tuple, Any
+
+@dataclass
+class SymbolInfo:
+    """Stores information related to a symbol"""
+
+    depth: int
+    lineno: int = None
+    type_: str = None
+    storage: int = None
+    symbol: Node = None
+    const: bool = False
+    value: Any = None
+    uses: list = field(default_factory=list)
+
+
+class SymbolTable:
+    """Stores all identifiers, literals and information
+    related to them"""
+
+    def __init__(self):
+        self.mapping: Dict[Tuple[str, int], SymbolInfo] = {}
+        self.depth = 0
+
+    def add(self, symbol: str) -> SymbolInfo:
+        if (symbol, self.depth) in self.mapping:
+            return self.mapping[symbol, self.depth]
+
+        new_symbol = SymbolInfo(self.depth)
+        self.mapping[symbol, self.depth] = new_symbol
+
+        return new_symbol
+
+    def update_info(self, symbol: str, lineno, type_=None, const=None, value=None):
+        sym = self.mapping[symbol, self.depth]
+        sym.lineno = lineno
+        sym.type_ = type_
+        # TODO: infer type from value if not given
+        # and set storage appropriately
+        # sym.storage = types[type_][1]
+        if value is not None:
+            sym.value = value
+
+        if const is not None:
+            sym.const = const
+
+    def check_exists(self, symbol) -> bool:
+        return (symbol, self.depth) in self.mapping
+
+    def is_declared(self, symbol) -> bool:
+        return (
+            self.check_exists(symbol)
+            and self.mapping[symbol, self.depth].lineno is not None
+        )
+
+    def get_if_exists(self, symbol) -> SymbolInfo:
+        return self.mapping.get((symbol, self.depth), None)
+
+    def __str__(self):
+        return str(
+            tabulate(
+                [
+                    [
+                        key[0],
+                        key[1],
+                        value.lineno,
+                        value.type_,
+                        value.storage,
+                        value.value,
+                        value.uses,
+                    ]
+                    for key, value in self.mapping.items()
+                ],
+                headers=["Symbol", "Depth", "Line No.", "Type", "Storage", "Value", "Uses"],
+                tablefmt="fancy_grid",
+            )
+        )
