660f4490b6696a23827b8b7f12fff46b4aa3e549
Srinidhi Temkar <srinidhitemkar6699@gmail.com>
2021-02-18 17:30:19 +0530

    modified test files to have package and func declarations

--- tests/operators.go
+++ tests/operators.go
@@ -1,9 +1,13 @@
-1 + 5
-3 - 9
-a + b
-b - c
-a * b
-a / b
-a * 4
-4 / b
-4 * 5 + 9 / 3 - (a / b)
+package operators
+
+func main() {
+	1 + 5
+	3 - 9
+	a + b
+	b - c
+	a * b
+	a / b
+	a * 4
+	4 / b
+	4 * 5 + 9 / 3 - (a / b)
+}
