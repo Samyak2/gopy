0068fcadb1383c4ddee578aa0868ed5b1ad892aa
Samyak S Sarnayak <samyak201@gmail.com>
2021-02-25 19:07:43 +0530

    parser - AST for const, var decl. better AST printing
    AST is not printed to a syntax_tree.txt file in the same directory.

--- syntree.py
+++ syntree.py
@@ -7,10 +7,13 @@ class Node:
         self.data = kwargs.get("data", None)
 
     def __repr__(self):
-        return f"<Node: {self.name}; {self.data}; {self.children}>"
+        return str(self)
 
     def __str__(self):
-        return f"<Node: {self.name}; {self.data}; {len(self.children)} child(s)>"
+        if self.data is not None:
+            return f"<{self.name}: {str(self.data)}>"
+        else:
+            return f"<{self.name}>"
 
     def add_child(self, child):
         if child is not None:
@@ -41,6 +44,8 @@ class UnaryOp(Node):
     """Node for unary operations"""
 
     def __init__(self, operator, operand):
+        if isinstance(operand, UnaryOp) and operand.operator is None:
+            operand = operand.operand
         super().__init__(f"Unary {operator}", children=[operand], data=operator)
 
     @property
@@ -50,6 +55,16 @@ class UnaryOp(Node):
     @property
     def operand(self):
         return self.children[0]
+
+
+class PrimaryExpr(Node):
+    """Node for PrimaryExpr
+
+    Ref: https://golang.org/ref/spec#PrimaryExpr
+    """
+
+    def __init__(self, operand):
+        super().__init__("PrimaryExpr", children=[], data=operand)
 
 
 class Literal(Node):
@@ -75,13 +90,31 @@ class List(Node):
         super().__init__("LIST", children=children)
         self.append = self.add_child
 
+    def __iter__(self):
+        return iter(self.children)
+
+
+class Signature(Node):
+    """Node to store function signature"""
+
+    def __init__(self, parameters, result=None):
+        super().__init__("signature", children=[parameters, result])
+
+    @property
+    def parameters(self):
+        return self.children[0]
+
+    @property
+    def result(self):
+        return self.children[1]
+
 
 class Function(Node):
     """Node to store function declaration"""
 
     def __init__(self, name, signature, body=None):
         super().__init__("FUNCTION", children=[signature, body], data=name)
-        self.name = name
+        self.fn_name = name
 
     @property
     def signature(self):
@@ -96,7 +129,7 @@ class Function(Node):
 
 
 class Type(Node):
-    "Parent class for all types"""
+    """Parent class for all types"""
 
 
 class Array(Type):
@@ -109,6 +142,48 @@ class Array(Type):
     @property
     def length(self):
         return self.children[0]
+
+
+class Identifier(Node):
+    """Node for identifiers"""
+
+    def __init__(self, ident_tuple):
+        super().__init__("IDENTIFIER", children=[], data=ident_tuple[1])
+
+
+class QualifiedIdent(Node):
+    """Node for qualified identifiers"""
+
+    def __init__(self, package_name, identifier):
+        super().__init__("IDENTIFIER", children=[], data=(package_name, identifier))
+
+
+class VariableDecl(Node):
+    """Node to store variable and constant declarations"""
+
+    def __init__(self, identifier_list, type_=None, expression_list=None, const=False):
+        super().__init__(
+            "DECL",
+            children=[identifier_list, expression_list],
+            data=(type_, const),
+        )
+
+    @property
+    def expression_list(self):
+        return self.children[1]
+
+    @property
+    def identifier_list(self):
+        return self.children[0]
+
+    @property
+    def type_(self):
+        return self.data[0]
+
+    @property
+    def is_const(self):
+        return self.data[1]
+
 
 # class OldNode:
 #     """Class to store a node of the AST"""
@@ -166,5 +241,3 @@ class Array(Type):
 #                 self.children.append(Node(type(child), node=child))
 #             else:
 #                 self.children.append(child)
-
-
