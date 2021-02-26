90993126fe38f8b9fb6825d8be4943162c7feb2f
Srinidhi Temkar <srinidhitemkar6699@gmail.com>
2021-02-26 20:58:52 +0530

    lexer, parser - added support for return,break,continue,if,for statements

---
+++ tests/if.go
@@ -0,0 +1,23 @@
+package iff
+
+func main() {
+	x,y,z,min,max := 1,2,3,4,5
+
+	if x > max {
+		if(y<min) {
+			return
+		} else if z {
+			return true
+		} else {
+			a := 20
+		}
+	}
+
+	if x := 25; x < y {
+		return x
+	} else if x > z {
+		return z
+	} else {
+		return y
+	}
+}
