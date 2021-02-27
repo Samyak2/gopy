62fb1cfad2c7ab7881848ba6d25cda3f52532840
RaghavAggarwal0310 <raghavaggarwal03.ra@gmail.com>
2021-02-27 07:58:59 +0530

    Removed the warning in parser

--- go_parser.py
+++ go_parser.py
@@ -7,7 +7,7 @@ from colorama import Fore, Style
 from ply import yacc
 
 import go_lexer
-from go_lexer import tokens, lex, find_column, symtab
+from go_lexer import new_tokens as tokens, lex, find_column, symtab
 import utils
 from utils import print_error, print_line, print_marker
 import syntree
