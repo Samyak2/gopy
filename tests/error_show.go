f690c487f5f47dfbfca545e8ceae6e4021c635fe
Samyak S Sarnayak <samyak201@gmail.com>
2021-04-16 00:16:27 +0530

    tests - update error_show test file

--- tests/error_show.go
+++ tests/error_show.go
@@ -2,11 +2,24 @@ package main
 
 var a = 5.0
 var b = 3
-var c = a + b
+var c = a + 3
 
 var d float64 = b + 3
 
 func main() {
     if a + 5 {
+        a = 5
     }
+
+    if 3 > a {
+        b = 5
+    }
+
+    break
+
+    for a > 3 {
+        a = 5
+    }
+
+    var e int
 }
